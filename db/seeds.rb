0c828d48d514b60e5bbc9593a3260c4e54ef4f6b
Douglas Berkley <douglasmberkley@gmail.com>
2021-05-13 17:32:35 +0900

    updated seeds to attach photos

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,8 +4,40 @@ puts "Cleaning DB..."
 List.destroy_all
 Movie.destroy_all
 
-15.times do
-  List.create!(name: Faker::Music.unique.genre)
+lists = [
+  { name: 'Classics', url: 'https://images.emojiterra.com/openmoji/v12.2/128px/1f3b9.png' },
+  { name: 'Horror', url: 'https://images.emojiterra.com/openmoji/v12.2/128px/1f52a.png' },
+  { name: 'Action', url: 'https://images.emojiterra.com/openmoji/v12.2/128px/1f4a3.png' },
+  { name: 'Comedy', url: 'https://images.emojiterra.com/openmoji/v12.2/128px/1f923.png' },
+  { name: 'Romantic', url: 'https://images.emojiterra.com/openmoji/v12.2/128px/1f618.png' },
+  { name: 'Stallone', url: 'https://images.emojiterra.com/openmoji/v12.2/128px/1f4aa.png' },
+  { name: 'Adult', url: 'https://images.emojiterra.com/openmoji/v12.2/128px/1f346.png' },
+  { name: 'Martial Arts', url: 'https://images.emojiterra.com/openmoji/v12.2/128px/1f94b.png' },
+  { name: 'Animated', url: 'https://images.emojiterra.com/openmoji/v12.2/128px/1f58c.png' },
+  { name: 'Epic', url: 'https://images.emojiterra.com/openmoji/v12.2/128px/2694.png' },
+  { name: 'Animal', url: 'https://images.emojiterra.com/openmoji/v12.2/128px/1f408.png' },
+  { name: 'Female Leads', url: 'https://images.emojiterra.com/openmoji/v12.2/128px/1f9b8-2640.png' },
+  { name: 'Space Opera', url: 'https://images.emojiterra.com/openmoji/v12.2/128px/1f680.png' },
+  { name: 'Sci-fi', url: 'https://images.emojiterra.com/openmoji/v12.2/128px/1f47d.png' },
+  { name: 'Religious', url: 'https://images.emojiterra.com/openmoji/v12.2/128px/1f47c.png' },
+  { name: 'Drugs', url: 'https://images.emojiterra.com/openmoji/v12.2/128px/1f92a.png' },
+  { name: 'Western', url: 'https://images.emojiterra.com/openmoji/v12.2/128px/1f920.png' },
+  { name: 'Scary', url: 'https://images.emojiterra.com/openmoji/v12.2/128px/1f47b.png' },
+  { name: 'Cerebral', url: 'https://images.emojiterra.com/openmoji/v12.2/128px/1f92f.png' },
+  { name: 'Japanese', url: 'https://images.emojiterra.com/openmoji/v12.2/128px/1f3ee.png' },
+  { name: 'LGBTQ+', url: 'https://images.emojiterra.com/openmoji/v12.2/128px/1f3f3-1f308.png' },
+  { name: 'Thriller', url: 'https://images.emojiterra.com/openmoji/v12.2/128px/1f575.png' },
+  { name: 'Fantasy', url: 'https://images.emojiterra.com/openmoji/v12.2/128px/1f9d9.png' },
+  { name: 'Disaster', url: 'https://images.emojiterra.com/openmoji/v12.2/128px/1f32a.png' },
+  { name: 'Travel', url: 'https://images.emojiterra.com/openmoji/v12.2/128px/2708.png' },
+  { name: 'Sports', url: 'https://images.emojiterra.com/openmoji/v12.2/128px/26bd.png' }
+]
+
+lists.each do |list_info|
+  list = List.create!(name: list_info[:name])
+  puts "Getting photo for #{list_info[:name]}..."
+  file = URI.open(list_info[:url])
+  list.photo.attach(io: file, filename: 'emoji.png', content_type: 'image/png')
 end
 
 
